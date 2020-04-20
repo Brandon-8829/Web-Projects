e9d31372e9d66f04f9050733b095d79fc1ff50da
Brandon-8829 <58235449+Brandon-8829@users.noreply.github.com>
2020-04-19 21:51:51 -0400

    Add files via upload

---
+++ Weather-application_v2/weatherApp.js
@@ -0,0 +1,32 @@
+let city = 'London';
+const api = 'http://api.openweathermap.org/data/2.5/weather?q=';
+const apiKey = '&appid=7330e2fdd5a5cb37fb74efb58e44552e&units=metric';
+
+function ask()  {
+  	city = document.getElementById("myInput").value;
+	let temperatureDescription = document.querySelector(".temperature-desription");
+	let temperatureDegree = document.querySelector(".temperature-degree");
+	let locationTimezone = document.querySelector(".location-timezone");
+	let pressureDescription = document.querySelector(".pressure-text");
+	let humidityDescription = document.querySelector(".humidity-text");
+	
+  	var url = api+city+apiKey;
+	fetch(url).then(response =>{
+		return response.json();
+	})
+	.then(data =>{
+		console.log(data);
+		
+		//set DOM elements from the API
+		var iconurl = "http://openweathermap.org/img/w/"+data.weather[0].icon+".png";
+		$('#wicon').attr('src', iconurl);
+		$('#temperature-desription').attr('textContent', data.weather[0].description);
+		temperatureDegree.textContent = data.main.temp;
+		//temperatureDescription.textContent = data.weather[0].description;
+		locationTimezone.textContent = data.timezone+" "+data.name+", "+data.sys.country;
+		pressureDescription.textContent = data.main.pressure+" Kpa";
+		humidityDescription.textContent = data.main.humidity+"%";
+		//$('#wicon').attr('src', iconurl);
+	});
+ }
+//7330e2fdd5a5cb37fb74efb58e44552e
